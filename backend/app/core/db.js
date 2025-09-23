a32f6be36acd675ca4dbc1b5390bb9667969b2d6
thomas rousseau <thomas.rousseau19@gmail.com>
2025-09-23 11:21:52 +0200

    user+strategy class complete

--- backend/app/core/db.js
+++ backend/app/core/db.js
@@ -1,1 +1,15 @@
 //connexion à la base de données (PostgreSQL, Mongo, etc.).
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'learn2trade',
+  process.env.DB_USER || 'l2t_user',
+  process.env.DB_PASS || 'l2t_pass',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+  }
+);
+
+export default sequelize;
