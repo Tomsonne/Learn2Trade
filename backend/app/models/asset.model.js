238a18a118a4496ae92ab3abb4bb50765a541a7f
Pulon Julien <pulonjulien@gmail.com>
2025-09-25 10:54:23 +0200

    create asset.model.js

--- backend/app/models/asset.model.js
+++ backend/app/models/asset.model.js
@@ -1,1 +1,18 @@
-//actifs supportés (BTCUSD, EURUSD, etc.).
+//actifs supportés (BTCUSD, EURUSD, etc.).CREATE TABLE assets (
+import { DataTypes } from 'sequelize';
+import sequelize from '../core/db.js'; // connexion à Postgres
+
+const Asset = sequelize.define('Asset', {
+
+  symbol: {
+
+    type: DataTypes.STRING,
+    primaryKey: true, //remplace l'id
+  },
+  kind: {
+    type: DataTypes.Enum('crypto', 'forex', 'index'),
+    allowNull: false,
+  },
+  tableName: 'assets',
+  timestamps: false,
+});
