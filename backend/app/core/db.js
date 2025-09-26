f172961881cddbdddfc9a1c6342a797973bdaf2e
Pulon Julien <pulonjulien@gmail.com>
2025-09-26 11:26:54 +0200

    tests models

--- backend/app/core/db.js
+++ backend/app/core/db.js
@@ -1,15 +1,29 @@
-//connexion à la base de données (PostgreSQL, Mongo, etc.).
+// app/core/db.js
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'learn2trade',
-  process.env.DB_USER || 'learn2trade',
-  process.env.DB_PASS || 'l2t_pass',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    port: process.env.DB_PORT || 5432,
-  }
-);
+const DIALECT = process.env.DB_DIALECT || 'postgres';
+let sequelize;
+
+if (process.env.NODE_ENV === 'test' && DIALECT === 'sqlite') {
+  // ✅ Tests unitaires : SQLite en mémoire
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false,
+  });
+} else {
+  // ✅ Dev/Prod/Intégration Postgres
+  sequelize = new Sequelize(
+    process.env.PG_DATABASE,
+    process.env.PG_USER,
+    process.env.PG_PASSWORD,
+    {
+      host: process.env.PG_HOST || 'localhost',
+      port: +(process.env.PG_PORT || 5432),
+      dialect: 'postgres',
+      logging: false,
+    }
+  );
+}
 
 export default sequelize;
