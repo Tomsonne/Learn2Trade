ab42211ccd3fbc8903c2f68f357d57b46aee4a95
Pulon Julien <pulonjulien@gmail.com>
2025-10-02 11:47:49 +0200

    remove pro plan from config.js

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -3,6 +3,6 @@ import marketRoutes from './market.routes.js'
 
 const router = Router()
 // monte les routes sous /api/v1/market/
-router.use('/market', marketRoutes) // prix et forex
+router.use('/market', marketRoutes) // prix et forex ohlc marketchartrange
 
 export default router
